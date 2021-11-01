dc7c452af943afc74e5d85869d81a8eb95c4ad03
Welton Cardoso <bryan_july@hotmail.com>
2021-11-01 16:14:01 -0300

    Finalizado site

--- script.js
+++ script.js
@@ -9,3 +9,39 @@ window.addEventListener('scroll', ()=>{
        header.classList.remove('scroll') /*As formas de escrever do if e else e diferente mas chama a mesma coisa*/
    }
 })
+
+
+let currentSlide = 0;
+
+let totalSlides = document.querySelectorAll('.slider--video').length;
+
+document.querySelector('.slider--width').style.width = `calc(600 * ${totalSlides} )`
+
+
+
+function goPrev(){
+    currentSlide--;
+    if(currentSlide < 0) {
+        currentSlide = totalSlides - 1;
+    }
+    updateMargin();
+
+}
+
+function goNext(){
+    currentSlide++;
+    if(currentSlide > (totalSlides-1)){
+        currentSlide = 0;
+    }
+    updateMargin();
+
+}
+
+function updateMargin(){
+    let newMargin = (currentSlide * 600) ;
+    document.querySelector('.slider--width').style.marginLeft = `-${newMargin}px`;
+
+
+}
+
+setInterval(goNext, 5000);
